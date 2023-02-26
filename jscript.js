51cc216a1863d1d3d362a0f3fc74bf212aef9b02
Anastasiya Soloveva <102226072+Solonasty@users.noreply.github.com>
2023-02-26 19:38:01 +0300

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -23,4 +23,73 @@ function applyFill(slider) {
 			0.1}%)`;
 	slider.style.background = bg;
 	sliderValue.setAttribute("data-length", slider.value);
+    console.log(slider.value)
 }
+
+// REGISTRATION
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', store);
+
+function store(event){
+    event.preventDefault();
+    let name = document.getElementById('name');
+    let tel = document.getElementById('tel');
+    let temp = document.getElementById('slider');
+    let comments = document.getElementById('comments');
+
+    if(name.value.length == 0){
+        alert('Please fill in name');
+
+    }else if(tel.value.length == 0){
+        alert('Please fill in phone number');
+
+    }else{
+
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('tel', tel.value);
+        localStorage.setItem('temp', temp.value);
+        localStorage.setItem('comments', comments.value);
+        console.log('Your account has been created');
+    // }
+}
+}
+
+
+// let phone = document.getElementById('phone');
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ - __ - __",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    tel.addEventListener("input", mask, false);
+    tel.addEventListener("focus", mask, false);
+    tel.addEventListener("blur", mask, false);
+    tel.addEventListener("keydown", mask, false)
+
+  });
+
+});
